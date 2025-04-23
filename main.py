32e0ea664bcd1b70e6e86157e12ac5690d45d181
MaxChou1205 <v2asault@yahoo.com.tw>
2025-04-23 23:58:29 +0800

    add linkin scrape

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-import os
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_ollama import ChatOllama
+
+from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == "__main__":
-    print("hello world")
-    print(os.getenv("OPEN_API_KEY"))
+    load_dotenv()
+
+    print("Hello LangChain")
+
+    summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    llm = ChatOllama(temperature=0, model="gemma3:latest")
+
+    chain = summary_prompt_template | llm
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+    )
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
