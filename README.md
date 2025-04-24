02e6242f2f26cada44332a50d7fa50de6a5df1f4
MaxChou1205 <v2asault@yahoo.com.tw>
2025-04-25 00:17:19 +0800

    finish ice breaker v1

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Ice Breaker
 
+### tools
+* https://app.tavily.com/home
+
 ### reference
-[repo](https://github.com/emarco177/ice_breaker)
+* [repo](https://github.com/emarco177/ice_breaker)
+
--- ice_breaker/main.py
+++ ice_breaker/main.py
@@ -1,32 +1,34 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
-# noinspection PyInterpreter
 from langchain_ollama import ChatOllama
+from ice_breaker.agents.linkedin_lookup_agent import linkedin_lookup_agent
+from ice_breaker.third_parties.linkedin import scrape_linkedin_profile
 
-from third_parties.linkedin import scrape_linkedin_profile
 
-if __name__ == "__main__":
-    load_dotenv()
-
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
     given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
-
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     llm = ChatOllama(temperature=0, model="gemma3:latest")
 
     chain = summary_prompt_template | llm
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
-    )
+
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco")
