be58df07dc95eb3bab79e9f421c555b662a8b1b7
MaxChou1205 <v2asault@yahoo.com.tw>
2025-05-11 18:31:58 +0800

    vector dbs finished

--- README.md
+++ README.md
@@ -5,11 +5,15 @@
 * https://app.tavily.com/home
 * log: https://smith.langchain.com/
 
+#### 向量資料庫工具
+* [pinecone (online)](https://www.pinecone.io/)
+* [faiss (offline)](https://faiss.ai/)
+
 ### reference
 
 * [repo](https://github.com/emarco177/ice_breaker)
 * [langsmith hub](https://smith.langchain.com/hub)
 * [victor dbs](https://medium.com/@EjiroOnose/vector-database-what-is-it-and-why-you-should-know-it-ae7e7dca82a4)
-* [pinecone](https://www.pinecone.io/)
 * [document-loaders](https://python.langchain.com/docs/concepts/document_loaders/)
 * [text-splitters](https://python.langchain.com/docs/how_to/#text-splitters)
+
--- vector_dbs/main.py
+++ vector_dbs/main.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_pinecone import PineconeVectorStore
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import ChatOllama
@@ -11,6 +12,11 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
 load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join([doc.page_content for doc in docs])
+
 
 if __name__ == "__main__":
     print(" Retrieving...")
@@ -36,3 +42,28 @@ if __name__ == "__main__":
     result = retrival_chain.invoke(input={"input": query})
 
     print(result)
+
+    template = """
+            Use the following pieces of context to answer the question at the end.
+            If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+            User three sentences maximum and keep the answer as concise as possible.
+            Always say "thanks for asking" at the end of your answer.
+            
+            {context}
+            
+            Question: {question}
+            
+            Answer:
+            """
+
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
+
+    rag_chain = (
+            {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+            | custom_rag_prompt
+            | llm
+    )
+
+    res = rag_chain.invoke(query)
+
+    print(res)
