e2eedc17fdbb90728057f7ad66120313e347e175
MaxChou1205 <v2asault@yahoo.com.tw>
2025-05-12 23:10:11 +0800

    add memorize

--- document_helper/backend/core.py
+++ document_helper/backend/core.py
@@ -1,4 +1,6 @@
 import os
+from typing import List, Dict, Any
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +10,10 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_pinecone import PineconeVectorStore
 from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     print(os.getenv("INDEX_NAME"))
     embeddings = OllamaEmbeddings(model="qwen3:latest")
     docsearch = PineconeVectorStore(index_name=os.getenv("INDEX_NAME"), embedding=embeddings)
@@ -19,10 +22,15 @@ def run_llm(query: str):
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
     )
-    result = qa.invoke(input={"input": query})
+
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
     # return result
     new_result = {
         "query": result["input"],
